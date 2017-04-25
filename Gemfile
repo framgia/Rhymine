15fa89bc43aadf4171a74115c2de9bd335bf7d5a
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-04-25 13:46:20 +0700

    Login & Logout

--- Gemfile
+++ Gemfile
@@ -15,6 +15,10 @@ gem "jquery-rails"
 gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
 gem "bootstrap-sass", "3.3.6"
+gem "bcrypt"
+gem "faker"
+gem "config"
+gem "devise"
 
 group :development, :test do
   gem "byebug", platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root "pages#show", page: "home"
   get "/pages/:page" => "pages#show"
+
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
 end
