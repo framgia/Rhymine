c51105fb33d9c6d9083b6ea039ad4ada5b36e3d9
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-05-09 17:13:42 +0700

    Add new song

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,24 @@
+class SongsController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+    @song = Song.new
+    @genres = Genre.all
+  end
+
+  def create
+    @song = current_user.songs.new song_params
+    if @song.save
+      flash[:info] = t "flash.info.upload_song_success"
+      redirect_to current_user
+    else
+      render :new
+    end
+  end
+
+  private
+  def song_params
+    params.require(:song).permit :title, :artist, :description,
+      :audio, :genre_id
+  end
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -6,4 +6,10 @@ class Song < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :reports, dependent: :destroy
+
+  mount_uploader :audio, AudioUploader
+
+  validates :title, presence: true
+  validates :artist, presence: true
+  validates :genre, presence: true
 end
