b0bb6f2e375668012f241420ab7cfd91d529f78f
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-05-26 09:32:33 +0700

    Show homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def show
     if valid_page?
+      @song_feeds = Song.feeds(current_user).order created_at: :desc
       render template: "pages/#{params[:page]}"
     else
       render file: "public/404.html", status: :not_found
